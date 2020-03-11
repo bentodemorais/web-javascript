6ae50713893da2d1d08bc77f827d971e668adb06
André Bento <bentodemorais@gmail.com>
2020-03-11 20:43:02 -0300

    funcao callBakEnd

--- 3.funcao/12.callBack2.js
+++ 3.funcao/12.callBack2.js
@@ -1,6 +1,6 @@
 const notas = [7.7, 6.5, 8.9, 3.6, 7.1, 9.0]
 
-// sem callback
+// sem callBack
 let notasBaixas = []
 for ( let i in notas ) {
     if ( notas[i] < 7 ) {
@@ -17,8 +17,12 @@ notasBaixas2 = notas.filter( function (nota) {
 
 console.log(notasBaixas2)
 
+// usando arrow
 const notasBaixas3 = notas.filter( nota => nota < 7 )
 console.log(notasBaixas3)
 
-
+// reuso de codigo
+const notasMenorque7 = nota => nota < 7
+const notasBaixas3 = notas.filter( notasMenorque7 )
+console.log(notasBaixas3)
 
